3dd8b28f6c69086ce642ac672cd732ffdceb6114
Anurag Chauhan <68834844+anuragstark@users.noreply.github.com>
2025-05-17 17:13:56 +0530

    Update test.py
    email notification on failed

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 
 def pookie():
-    print("Hello Honey- Did you know that Jenkins works so hard, it probably dreams in pipelines? - Hanji ")
+    prin("Hello Honey- Did you know that Jenkins works so hard, it probably dreams in pipelines? - Hanji ")
 
 if __name__ == "__main__":
     pookie()
